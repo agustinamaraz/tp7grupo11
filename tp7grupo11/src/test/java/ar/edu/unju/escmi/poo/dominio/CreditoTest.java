b61226ed8afbfd3c589028427740db581619031b
Agustina Maraz <Agustina Maraz@192.168.0.12>
2022-11-13 12:12:58 -0300

    Prueba unitaria para verificar si se crearon las 30 cuotas de Credito

---
+++ tp7grupo11/src/test/java/ar/edu/unju/escmi/poo/dominio/CreditoTest.java
@@ -0,0 +1,42 @@
+package ar.edu.unju.escmi.poo.dominio;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import ar.edu.unju.escmi.poo.collections.CollectionCliente;
+import ar.edu.unju.escmi.poo.collections.CollectionCredito;
+import ar.edu.unju.escmi.poo.collections.CollectionFactura;
+import ar.edu.unju.escmi.poo.collections.CollectionProducto;
+import ar.edu.unju.escmi.poo.collections.CollectionStock;
+import ar.edu.unju.escmi.poo.collections.CollectionTarjetaCredito;
+
+class CreditoTest {
+	Credito credito;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		CollectionCliente.getClientes();
+		CollectionTarjetaCredito.getTarjetas(); //necesita clientes por eso se debe invocar antes a la collectionCliente
+		CollectionProducto.getProductos(); 
+		CollectionStock.getStocks(); // necesita productos
+		CollectionFactura.getFacturas(); //necesita detalles que a su vez necesita productos
+		CollectionCredito.getCreditos(); // necesita tarjeta de credito y factura
+		
+		credito=CollectionCredito.creditos.get(0);
+		
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+	
+	//Test que permita validar que la lista de cuotas que se generan no es un valor null
+	@Test
+	void testGenerarCuotas() {
+		assertEquals(30,credito.getCuotas().size());
+	}
+
+}
